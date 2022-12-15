41bce38be0f43f03982a424d9b5f9d73d4a74109
Matheus B <matheus_ob59@hotmail.com>
2022-12-15 14:58:17 -0300

    Create base notification service.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { Notification, Prisma } from '@prisma/client';
+import { randomUUID } from 'crypto';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async listAllNotifications(): Promise<Notification[]> {
+    const notifications = await this.prisma.notification.findMany();
+
+    return notifications;
+  }
+
+  @Post()
+  async createNotification(): Promise<Notification> {
+    const notification = await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Your product has arrived at transportation.',
+        category: 'Logistics',
+        recipientId: randomUUID(),
+      },
+    });
+
+    return notification;
   }
 }
