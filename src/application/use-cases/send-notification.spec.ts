39e3393b3453094399d2a978b2ef065fa2e58884
Matheus B <matheus_ob59@hotmail.com>
2022-12-16 22:38:53 -0300

    Create notifications repository contract and test using in memory storage.

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,8 +1,10 @@
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
 import { SendNotification } from './send-notification';
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const notificationRepository = new InMemoryNotificationsRepository();
+    const sendNotification = new SendNotification(notificationRepository);
 
     const { notification } = await sendNotification.execute({
       content: 'New notification being tested.',
@@ -10,6 +12,6 @@ describe('Send notification', () => {
       recipientId: 'some-unique-id',
     });
 
-    expect(notification).toBeTruthy();
+    expect(notificationRepository.notifications[0]).toBe(notification);
   });
 });
